d15a1f22fdd21c8126160f2596c99c76f3ea14c3
quininer kel <quininer@live.com>
2016-08-29 15:42:22 +0800

    support no_std

--- src/colorhash256.rs
+++ src/colorhash256.rs
@@ -1,11 +1,18 @@
+#![no_std]
+
 //! Like Chroma-Hash, but with ANSI terminal colors.
-extern crate sodiumoxide;
-use sodiumoxide::crypto::hash::sha256;
+extern crate tiny_keccak;
+use tiny_keccak::Keccak;
 
 /// Hashes given bytes and encodes the result as ANSI terminal colors.
 pub fn hash_as_ansi(bytes: &[u8]) -> [u16; 8] {
-    let sha256::Digest(hash) = sha256::hash(bytes);
-    let mut colors: [u16; 8] = [0; 8];
+    let mut colors = [0; 8];
+    let mut hash = [0; 8];
+
+    let mut sha3 = Keccak::new_sha3_256();
+    sha3.update(bytes);
+    sha3.finalize(&mut hash);
+
     for i in 0..8 {
         colors[i] = 16 + (hash[i] as u16 % 216);
     }
@@ -29,11 +36,11 @@ mod tests {
 
     #[test]
     fn test_ansi() {
-        assert_eq!(hash_as_ansi(b"Correct Horse Battery Staple"), [191, 35, 175, 178, 148, 70, 82, 37]);
+        assert_eq!(hash_as_ansi(b"Correct Horse Battery Staple"), [79, 131, 17, 194, 196, 52, 19, 54]);
     }
 
     #[test]
     fn test_rgb() {
-        assert_eq!(hash_as_rgb(b"Correct Horse Battery Staple"), [(204, 255, 51), (0, 153, 51), (204, 102, 153), (204, 153, 0), (153, 204, 0), (51, 153, 0), (51, 255, 0), (0, 153, 153)]);
+        assert_eq!(hash_as_rgb(b"Correct Horse Battery Staple"), [(51, 204, 153), (153, 51, 51), (0, 0, 51), (204, 255, 204), (255, 0, 0), (51, 0, 0), (0, 0, 153), (51, 0, 102)]);
     }
 }
